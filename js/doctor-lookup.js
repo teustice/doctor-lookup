df4ac07c4d1a241774bedc8328162ee3cabaf373
Tanner Eustice <teustice@msn.com>
2017-05-26 09:43:26 -0700

    create object from api response

--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -1,9 +1,18 @@
 var apiKey = require('./../.env').apiKey;
 
+var doctors = [];
 exports.getDoctors = function(medicalIssue) {
+  doctors = [];
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-      console.log(result);
+      result.data.forEach(function(doctor){
+        doctors.push({
+          title: doctor.profile.title,
+          firstName: doctor.profile.first_name,
+          picture: doctor.profile.image_url
+        });
+      });
+      console.log(doctors);
     })
    .fail(function(error){
       console.log("fail");
