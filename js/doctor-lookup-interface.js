8ed43366a6002dc8ddffb6ebba4ffaf78a496753
Tanner Eustice <teustice@msn.com>
2017-05-26 14:17:34 -0700

    add dropdown for all specialties

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,36 +1,53 @@
 getDoctors = require('./../js/doctor-lookup.js').getDoctors;
+getSpecialties = require('./../js/doctor-lookup.js').getSpecialties;
 
 var displayDoctors = function(doctors) {
-  console.log(doctors);
-  specialties = [];
-  if (doctors.length === 0){
+  $('#output').text("");
+  if (doctors.length === 0) {
     $('#output').append(`<h3>There are no doctors in the area that specialize in <em>${userInput}</em></h3>`);
   } else {
-    doctors.forEach(function(doctor){
+    doctors.forEach(function(doctor) {
       $('#output').append(
         "<div class='container'>" +
-          "<div class='col-md-4'>" +
-            "<img src='" + doctor.picture + "'/>" +
-          "</div>" +
-          "<div class='col-md-4'>" +
-            "<h3>" + doctor.firstName + "</h3>" +
-          "</div>" +
-          "<div class='col-md-4'>" +
-            "<h3>" + doctor.title + "</h3>" +
-          "</div>" +
+        "<div class='col-md-4'>" +
+        "<img src='" + doctor.picture + "'/>" +
+        "</div>" +
+        "<div class='col-md-4'>" +
+        "<h3>" + doctor.firstName + "</h3>" +
+        "</div>" +
+        "<div class='col-md-4'>" +
+        "<h3>" + doctor.title + "</h3>" +
+        "</div>" +
         "</div>");
     });
   }
 };
 
-$(function(){
-  $('#doctor-form').submit(function(e){
+var displaySpecialties = function(specialties) {
+  specialties.forEach(function(specialty) {
+    $('#doctor-drop').append(
+      `<option value='${specialty}'>${specialty}</option>`
+    );
+  });
+};
+
+$(function() {
+  var specialties = getSpecialties();
+  setTimeout(function() {
+    displaySpecialties(specialties);
+  }, 1000);
+
+  $('#doctor-form').submit(function(e) {
     e.preventDefault();
-    userInput = $('#doctor-search').val();
+    if ($('#doctor-search').val() === "") {
+      userInput = $('#doctor-drop').val();
+    } else {
+      userInput = $('#doctor-search').val();
+    }
     $("#query").text("Doctors who specialize in: " + userInput);
     doctors = getDoctors(userInput);
     $('.labels').show();
-    setTimeout(function(){
+    setTimeout(function() {
       displayDoctors(doctors);
     }, 1000);
 
--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -1,13 +1,12 @@
 var apiKey = require('./../.env').apiKey;
 
-var doctors = [];
 exports.getDoctors = function(medicalIssue) {
-  doctors = [];
-  $('#output').text("");
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-     console.log("success");
-      result.data.forEach(function(doctor){
+  var doctors = [];
+
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+    .then(function(result) {
+      console.log("doctor success");
+      result.data.forEach(function(doctor) {
         doctors.push({
           title: doctor.profile.title,
           firstName: doctor.profile.first_name,
@@ -16,8 +15,23 @@ exports.getDoctors = function(medicalIssue) {
         });
       });
     })
-   .fail(function(error){
-      console.log("fail");
+    .fail(function(error) {
+      console.log("doctor fail");
     });
-    return doctors;
+  return doctors;
+};
+
+exports.getSpecialties = function() {
+  var specialties = [];
+  $.get('https://api.betterdoctor.com/2016-03-01/specialties?user_key=' + apiKey)
+    .then(function(result) {
+      console.log("specialty success");
+      result.data.forEach(function(specialty) {
+        specialties.push(specialty.name);
+      });
+    })
+    .fail(function(error) {
+      console.log("specialty fail");
+    });
+  return specialties;
 };
